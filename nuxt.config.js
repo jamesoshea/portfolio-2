2ec32062d1105f0ceaf8634cf26e3496d2099a65
James O'Shea <jamesoshea89@hotmail.com>
2018-06-10 09:11:06 +0200

    Add prettier, remove unnecessary boilerplate

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  css: [
+    '~/static/fictoan.css'
+  ],
   /*
   ** Customize the progress bar color
   */

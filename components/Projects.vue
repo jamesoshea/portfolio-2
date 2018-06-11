d2b2d087919a207b912b2db6a6d07f2fb6a4b99f
James O'Shea <jo@3yourmind.com>
2018-06-11 19:46:24 +0200

    fix layout on mobile

--- components/Projects.vue
+++ components/Projects.vue
@@ -2,36 +2,36 @@
   <div>
     <div id="about" class="ff-card padding-small text-justify margin-top-tiny">
       <h4>Projects</h4>
-        <table class="ff-table full-width padding-tiny">
-          <tbody>
-            <tr>
-              <td><a href="https://github.com/3YOURMIND/fire-pr">Fire-PR</a></td>
-              <td>
-                A tool we created at 3YOURMIND to standardise Pull Requests by
-                providing a standard template, and auto filling it using a
-                chrome extension written in Vue.
-              </td>
-              <td>
-                November 2017<br />
-                -<br />
-                Present<br />
-              </td>
-            </tr>
-            <tr>
-              <td class="text-left"><a href="https://github.com/jamesoshea/touchemin">Touchemin</a></td>
-              <td class="text-justify">
-                A Vue based PWA which uses Tone.js to play chord based on touch
-                screen interactions. Implements a filter and settings are coming
-                soon.
-              </td>
-              <td>
-                April 2018<br />
-                -<br />
-                Present<br />
-              </td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="row">
+        <div class="portion one-fourth">
+          <a href="https://github.com/3YOURMIND/fire-pr">Fire-PR</a>
+        </div>
+        <div class="portion two-fourths">
+          A tool we created at 3YOURMIND to standardise Pull Requests by
+          providing a standard template, and auto filling it using a
+          chrome extension written in Vue.
+        </div>
+        <div class="portion one-fourth">
+          November 2017<br />
+          -<br />
+          Present<br />
+        </div>
+      </div>
+      <div class="row">
+        <div class="portion one-fourth">
+          <a href="https://github.com/jamesoshea/touchemin">Touchemin</a>
+        </div>
+        <div class="portion two-fourths">
+          A Vue based PWA which uses Tone.js to play chords based on touch
+          screen interactions. Implements a filter and settings are coming
+          soon.
+        </div>
+        <div class="portion one-fourth">
+          April 2018<br />
+          -<br />
+          Present<br />
+        </div>
+      </div>
     </div>
   </div>
 </template>

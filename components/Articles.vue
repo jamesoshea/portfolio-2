1339a321d9cf0921b67013c3f684803f6c7ab391
James <jo@3yourmind.com>
2018-08-28 20:47:27 +0200

    fix articles layout

--- components/Articles.vue
+++ components/Articles.vue
@@ -3,7 +3,7 @@
     <div id="about" class="ff-card padding-small text-justify margin-top-tiny">
       <h4 class="margin-bottom-small">Articles</h4>
       <div class="row">
-        <div class="portion one-fourth">
+        <div class="portion">
           <a href="https://medium.com/3yourmind/testing-vue-components-a-cheat-sheet-299b3b8be88d">
             Testing Vue Components ­— a Cheat Sheet
           </a>
